5509d73310c938a8daa76290cfd2340bf96667f6
Thomas Corbett <thomas.corbett@miniclip.com>
2014-04-27 20:02:50 -0400

    Initial Commit

--- src/tomcorbett/UstreamApi/DataApi.php
+++ src/tomcorbett/UstreamApi/DataApi.php
@@ -1,4 +1,8 @@
 <?php
+namespace tomcorbett\UstreamApi;
+
+use GuzzleHttp\Client;
+
 /**
  * This class has been created to help developers adapt the UStream Services to their sites
  * 
@@ -6,37 +10,64 @@
  * @version 0.1a
  * @license GPL
  */
-class UStream {
-	
-	static public $apiUrl = 'http://api.ustream.tv'; //complete url reference http://api.ustream.tv/[html|json|xml|php]/[subject]/[subjectUID|scope]/[command]/[otherparams]/?page=[n]&limit=[l]&key=[devkey]
+class DataApi
+{
+	/**
+     * @var string
+     */
+	protected $apiUrl = 'http://api.ustream.tv'; //complete url reference http://api.ustream.tv/[html|json|xml|php]/[subject]/[subjectUID|scope]/[command]/[otherparams]/?page=[n]&limit=[l]&key=[devkey]
 	
 	/**
 	 * The main response method *** DO NOT CHANGE IT BECAUSE THE CLASS USES JSON TO COMMUNICATE ***
-	 * @var unknown_type
+     * 
+	 * @var string
 	 */
-	static private $responseMode = 'json'; //html, json, xml, php
+	protected $responseMode = 'json'; //html, json, xml, php
 	
 	/**
 	 * The main request mode you can change it using setRequestMode
-	 * @var String
+     * 
+	 * @var string
 	 */
-	static public $requestMode = 'channel'; //user, channel, stream, video, system
+	protected $requestMode = 'channel'; //user, channel, stream, video, system
 	
 	/**
-	 * Your UStream API Key
-	 * @var String
+	 * The UStream API Key
+     * 
+	 * @var string
 	 */
-	static protected $apiKey = null;
+	protected $apiKey = null;
 	
-	public $cacheResult = null;
+    /**
+     * @var array
+     */
+	protected $cacheResult = null;
 	
-	public $limit = 20;
-	public $page = 1;
+    /**
+     * Request client - using Guzzle for HTTP requests
+     * 
+     * @var GuzzleHttp\Client
+     */
+    protected $client;
+
+    /**
+     * The default limit
+     * 
+     * @var integer
+     */
+    protected $limit = 20;
+    
+    /**
+     * @var integer
+     */
+	protected $page = 1;
 	
-	public function __construct($apiKey, $requestMode = 'channel') {
-		self::$apiKey = $apiKey;
-		self::$requestMode = $requestMode;
-		$this->cacheResult = new stdClass();
+	public function __construct($apiKey, $requestMode = 'channel')
+    {
+		$this->apiKey       = $apiKey;
+		$this->requestMode  = $requestMode;
+		$this->cacheResult  = new stdClass();
+        $this->client       = new GuzzleHttp\Client();
 	}
 	
 	###########################################
@@ -49,7 +80,8 @@ class UStream {
 	 * @param String $subject
 	 * @return stdObj
 	 */
-	public function getInfo($subject) {
+	public function getInfo($subject)
+    {
 		$requestUrl = $this->buildRequestUrl($subject, 'getInfo');
 		
 		return $this->getResult($requestUrl);
@@ -63,7 +95,8 @@ class UStream {
 	 * 
 	 * @return stdClass
 	 */
-	public function getValueOf($subject, $property) {
+	public function getValueOf($subject, $property)
+    {
 		$requestUrl = $this->buildRequestUrl($subject, 'getValueOf/'.$property);
 				
 		return $this->getResult($requestUrl);
@@ -80,7 +113,8 @@ class UStream {
 	 * 
 	 * @return stdClass
 	 */
-	public function search($scope, $where, $how, $what,$page = 1) {
+	public function search($scope, $where, $how, $what,$page = 1)
+    {
 		$fullCommand = sprintf("search/%s:%s:%s", $where, $how, $what);
 		
 		$requestUrl = $this->buildRequestUrl($scope, $fullCommand);
@@ -97,7 +131,8 @@ class UStream {
 	 * 
 	 * @return stdClass
 	 */
-	public function getRecent() {
+	public function getRecent()
+    {
 		$requestUrl = $this->buildRequestUrl('all', 'getRecent');
 		
 		return $this->getResult($requestUrl);
@@ -108,7 +143,8 @@ class UStream {
 	 * 
 	 * @return stdClass
 	 */
-	public function getMostViewers() {
+	public function getMostViewers()
+    {
 		$requestUrl = $this->buildRequestUrl('all', 'getMostViewers');
 		
 		return $this->getResult($requestUrl);
@@ -119,7 +155,8 @@ class UStream {
 	 * 
 	 * @return stdClass
 	 */
-	public function getRandom() {
+	public function getRandom()
+    {
 		$requestUrl = $this->buildRequestUrl('all', 'getRandom');
 		
 		return $this->getResult($requestUrl);
@@ -131,7 +168,8 @@ class UStream {
 	 * @param String $page
 	 * @return stdClass
 	 */
-	public function getAllNew($page = 1) {
+	public function getAllNew($page = 1)
+    {
 		$requestUrl = $this->buildRequestUrl('all', 'getAllNew');
 		
 		return $this->getResult($requestUrl, $page);
@@ -147,7 +185,8 @@ class UStream {
 	 * @param String $subject
 	 * @return stdClass
 	 */
-	public function getTags($subject) {
+	public function getTags($subject)
+    {
 		$requestUrl = $this->buildRequestUrl($subject, 'getTags');
 		
 		return $this->getResult($requestUrl);
@@ -163,7 +202,8 @@ class UStream {
 	 * @param String $subject
 	 * @return stdClass
 	 */
-	public function getEmbedTag($subject) {
+	public function getEmbedTag($subject)
+    {
 		$requestUrl = $this->buildRequestUrl($subject, 'getEmbedTag');
 		
 		return $this->getResult($requestUrl);
@@ -180,7 +220,8 @@ class UStream {
 	 * 
 	 * @return stdClass
 	 */
-	public function getCustomEmbedTag($subject,$width = 100, $height = 100, $autoPlay = false, $mute = false) {
+	public function getCustomEmbedTag($subject,$width = 100, $height = 100, $autoPlay = false, $mute = false)
+    {
 		$parms['autoplay'] = $autoPlay;
 		$parms['mute'] = $mute;
 		$parms['width'] = $width;
@@ -202,7 +243,8 @@ class UStream {
 	 * 
 	 * @return stdClass
 	 */
-	public function getId($subject) {
+	public function getId($subject)
+    {
 		$requestUrl = $this->buildRequestUrl($subject, 'getId');
 		
 		return $this->getResult($requestUrl);
@@ -216,7 +258,8 @@ class UStream {
 	 * 
 	 * @return stdClass
 	 */
-	public function listAllChannels($subject, $page = 1) {
+	public function listAllChannels($subject, $page = 1)
+    {
 		$requestUrl = $this->buildRequestUrl($subject, 'listAllChannels');
 		
 		return $this->getResult($requestUrl, $page);
@@ -230,7 +273,8 @@ class UStream {
 	 * 
 	 * @return stdClass
 	 */
-	public function listAllVideos($subject, $page = 1) {
+	public function listAllVideos($subject, $page = 1)
+    {
 		$requestUrl = $this->buildRequestUrl($subject, 'listAllVideos');
 		
 		return $this->getResult($requestUrl, $page);
@@ -244,7 +288,8 @@ class UStream {
 	 * 
 	 * @return stdClass
 	 */
-	public function getComments($subject, $page = 1) {
+	public function getComments($subject, $page = 1)
+    {
 		$requestUrl = $this->buildRequestUrl($subject, 'getComments');
 		
 		return $this->getResult($requestUrl, $page);
@@ -260,7 +305,8 @@ class UStream {
 	 * @param String $channel
 	 * @return Boolean
 	 */
-	public function isLive($channel) {
+	public function isLive($channel)
+    {
 		$channelData = $this->getInfo($channel);
 		
 		if($channelData->results->status == 'live') {
@@ -278,7 +324,8 @@ class UStream {
 	 * @param Array $parms
 	 * @return String
 	 */
-	private function buildRequestUrl($subject, $command, array $parms = array()) {
+	private function buildRequestUrl($subject, $command, array $parms = array())
+    {
 		foreach($parms as $index => $value) {
 			$trueParms = !empty($trueParms) && $trueParms != null ? $trueParms .';' : $trueParms;
 			$trueParms .= sprintf("%s:%s",$index,$value);
@@ -286,7 +333,7 @@ class UStream {
 		
 		$trueParms = !empty($trueParms) && $trueParms != null ? '&params=' . $trueParms: $trueParms;
 		
-		return $this->calledUrl = sprintf("%s/%s/%s/%s/%s?key=%s%s",self::$apiUrl, self::$responseMode, self::$requestMode, $subject, $command, self::$apiKey, $trueParms);
+		return $this->calledUrl = sprintf("%s/%s/%s/%s/%s?key=%s%s", $this->apiUrl, $this->responseMode, $this->requestMode, $subject, $command, $this->apiKey, $trueParms);
 	}
 	
 	###########################################
@@ -301,11 +348,11 @@ class UStream {
 	 * 
 	 * @return stdClass
 	 */
-	public function getResult($requestUrl, $page = 1) {
-		
+	public function getResult($requestUrl, $page = 1)
+    {
 		$requestUrl = sprintf("$requestUrl&limit=%s&page=%s",$this->limit,$this->page);
 		
-		$this->cacheResult = json_decode(file_get_contents($requestUrl));
+        $this->cacheResult = json_decode($this->client->get($requestUrl)->getBody());
 		
 		if($this->error() == true) {
 			return false;
@@ -319,7 +366,8 @@ class UStream {
 	 * 
 	 * @return Boolean
 	 */
-	private function error() {
+	private function error()
+    {
 		if($this->cacheResult->error != null) {
 			return true;
 		} else {
@@ -332,7 +380,8 @@ class UStream {
 	 * 
 	 * @return Array
 	 */
-	public function getError() {
+	public function getError()
+    {
 		$return['code'] = $this->cacheResult->error;
 		$return['message'] = $this->cacheResult->msg;
 		
@@ -342,30 +391,96 @@ class UStream {
 	/**
 	 * Set the mode of response from UStream API
 	 * 
-	 * @param String $responseMode
-	 */
-	public function setResponseMode($responseMode) {
-		self::$responseMode = $responseMode;
+     * @param string $responseMode
+     * @return \tomcorbett\UstreamApi\DataApi
+     */
+	public function setResponseMode($responseMode)
+    {
+		$this->responseMode = $responseMode;
+        
+        return $this;
 	}
 	
 	/**
 	 * Set the request mode: channel, user, system, video, stream
 	 * 
-	 * @param String $requestMode
-	 */
-	public function setRequestMode($requestMode) {
-		self::$requestMode = $requestMode;
+     * @param string $requestMode
+     * @return \tomcorbett\UstreamApi\DataApi
+     */
+	public function setRequestMode($requestMode)
+    {
+		$this->requestMode = $requestMode;
+        
+        return $this;
 	}
 	
 	/**
-	 * Define your UStream API Key
-	 * 
-	 * @param String $apiKey
-	 */
-	public function setApiKey($apiKey) {
-		self::$apiKey = $apiKey;
+     * @param string $apiKey
+     * @return \tomcorbett\UstreamApi\DataApi
+     */
+	public function setApiKey($apiKey)
+    {
+		$this->$apiKey = $apiKey;
+        
+        return $this;
 	}
-	
+    
+    /**
+     * @return \GuzzleHttp\Client 
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+    
+    /**
+     * @param \GuzzleHttp\Client $client
+     * @return \tomcorbett\UstreamApi\DataApi
+     */
+    public function setClient(\GuzzleHttp\Client $client)
+    {
+        $this->client = $client;
+        
+        return $this;
+    }
+    
+    /**
+     * @return integer
+     */
+    public function getLimit()
+    {
+        return $this->limit;
+    }
+    
+    /**
+     * @param integer $limit
+     * @return \tomcorbett\UstreamApi\DataApi
+     */
+    public function setLimit($limit)
+    {
+        $this->limit = $limit;
+        
+        return $this;
+    }
+    
+    /**
+     * @return integer
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+    
+    /**
+     * @param integer $page
+     * @return \tomcorbett\UstreamApi\DataApi
+     */
+    public function setPage($page)
+    {
+        $this->page = $page;
+        
+        return $this;
+    }
 }
 
 ?>
